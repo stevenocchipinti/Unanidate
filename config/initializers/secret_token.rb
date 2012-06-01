935ba0f9428945333b8b5f5c18e88ae40d37736d
Steven Occhipinti <dev@stevenocchipinti.com>
2012-06-01 23:46:32 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unanidate::Application.config.secret_token = '448bf39a772a537c179903e64e11f5ea1eecd6016b34410705ec597d6f044143aed7f671d14fe94c3a932644fdec94d4a5f79724df6ae0b1b11b95052f1aa0e8'
