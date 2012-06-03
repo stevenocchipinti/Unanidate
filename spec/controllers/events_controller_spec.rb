d19c88fcb3f33cd176302de28fbf8c982279d273
Steven Occhipinti <dev@stevenocchipinti.com>
2012-06-03 21:10:16 +1000

    Add valid_attributes to the test to make RSpec work

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -24,9 +24,12 @@ describe EventsController do
   # Event. As you add validations to Event, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {
+      title: "Party time!",
+      description: "We're gunna throw a big party and stuff"
+    }
   end
-  
+
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # EventsController. Be sure to keep this updated too.
