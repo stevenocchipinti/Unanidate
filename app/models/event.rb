e06f990d758e9d46767367657023dbe0f27eb6ef
Steven Occhipinti <dev@stevenocchipinti.com>
2012-06-03 15:23:01 +1000

    Use Formtastic to generate a nested form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :title, :options_attributes
+  attr_accessible :description, :title, :selected_option_id, :options_attributes
   has_many :options, :dependent => :destroy
   accepts_nested_attributes_for :options
   belongs_to :selected_option, class_name: Option
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,8 @@
 class Option < ActiveRecord::Base
   attr_accessible :datetime
   belongs_to :event
+
+  def to_s
+    datetime.strftime("%D @ %T")
+  end
 end
