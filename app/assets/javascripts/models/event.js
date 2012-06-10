bce4a9ac3c8163166cae46e0270bbc7c1f9502f3
Steven Occhipinti <dev@stevenocchipinti.com>
2012-06-10 21:55:52 +1000

    Create EmberJS Model with DataStore

---
+++ app/assets/javascripts/models/event.js
@@ -0,0 +1,4 @@
+Unanidate.Event = DS.Model.extend({
+  title: DS.attr("string"),
+  description: DS.attr("string")
+});
--- app/assets/javascripts/unanidate.js
+++ app/assets/javascripts/unanidate.js
@@ -6,6 +6,12 @@
 //= require_tree ./states
 //= require_self
 
+// specify the adapter for accessing with ember-data
+Unanidate.store = DS.Store.create({
+  adapter: DS.RESTAdapter.create({bulkCommit: false}),
+  revision: 4
+})
+
 // Unanidate.stateManager is useful for debugging,
 // but don't use it directly in application code.
 var stateManager = Unanidate.stateManager = Unanidate.StateManager.create();
