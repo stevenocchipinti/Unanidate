d1b7353af33ecd36eccb1beb8ea26603d7c27e76
Steven Occhipinti <dev@stevenocchipinti.com>
2012-06-17 21:09:05 +1000

    Make RSpec as pending and remove test directory

--- spec/views/events/index.html.haml_spec.rb
+++ spec/views/events/index.html.haml_spec.rb
@@ -15,6 +15,7 @@ describe "events/index" do
   end
 
   it "renders a list of events" do
+    pending
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Title".to_s, :count => 2
