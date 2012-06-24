08ab7ff23d79944797d7d377fe773fdd66e2528a
Steven Occhipinti <dev@stevenocchipinti.com>
2012-06-24 21:33:11 +1000

    Adjust view spec to make it pass (basic tests)

--- spec/views/events/index.html.haml_spec.rb
+++ spec/views/events/index.html.haml_spec.rb
@@ -15,10 +15,9 @@ describe "events/index" do
   end
 
   it "renders a list of events" do
-    pending
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select ".accordion-heading > a", /Title/
+    assert_select ".accordion-inner > p", /MyText/
   end
 end
