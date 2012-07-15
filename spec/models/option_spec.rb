fec2c96310c586f87dca28f49efd24473526788e
Steven Occhipinti <dev@stevenocchipinti.com>
2012-07-15 15:52:05 +1000

    Refactor the option model spec to NOT hit the DB

--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -3,29 +3,31 @@ require 'spec_helper'
 describe Option do
 
   before(:each) do
-    @event = FactoryGirl.create :event_with_options
-    @option = @event.options.first
+    @event = FactoryGirl.build :event
+    @option = FactoryGirl.build :option, id: 1
   end
 
   it "has a valid factory" do
-    FactoryGirl.build(:option).should be_valid
+    @option.should be_valid
   end
 
   describe "#selected?" do
 
     it "returns true if this option is selected in its event" do
-      @event.selected_option = @option
-      @event.save
+      @option.stub(:event).and_return(@event)
+      @event.stub(:selected_option_id).and_return(@option.id)
       @option.selected?.should == true
     end
 
-    it "returns false if the event's selected_option is not this option" do
+    it "returns false if there is no associated event" do
       @option.selected?.should == false
     end
 
-    it "returns false if there is no associated event" do
-      option = FactoryGirl.build :option
-      option.selected?.should == false
+    it "returns false if the event's selected_option is not this option" do
+      # This would be an error state
+      @option.stub(:event).and_return(@event)
+      @event.stub(:selected_option_id).and_return(999)
+      @option.selected?.should == false
     end
 
   end
