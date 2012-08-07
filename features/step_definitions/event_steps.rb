3d099663d468841622e9380b0dcaca8af8150c04
Steven Occhipinti <dev@stevenocchipinti.com>
2012-08-07 22:38:55 +1000

    WIP: Add a 'create event' feature

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -12,11 +12,26 @@ When /^I navigate to the first event$/ do
   click_link 'Show'
 end
 
+When /^I create an event with the following details:$/ do |table|
+  visit events_path
+  click_link 'New'
+  table.rows_hash.each_pair do |key,value|
+    fill_in key, with: value
+  end
+  click_on 'Create Event'
+end
+
 Then /^I should see (\d+) events?$/ do |num_events|
   all('.accordion-group').count.should == num_events.to_i
 end
 
-Then /^I should see the event page$/ do
+Then /^I should see an event page$/ do
   page.should have_selector 'h1#event-title'
-  page.should have_selector '#description.well'
+  page.should have_selector '#description'
+end
+
+Then /^I should see an event page with the following details:$/ do |table|
+  page.should have_selector 'h1#event-title', text: table.rows_hash[:Title]
+  page.should have_selector '#description', text: table.rows_hash[:Description]
+  page.should have_selector '#th', text: 'Date'
 end
